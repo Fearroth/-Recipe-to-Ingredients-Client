d9faeb6a0a9dcb4fc725b88a156cad60152248e0
Daniel <karczmarczykdaniel@gmail.com>
2023-03-24 03:58:13 +0100

    add2 basic functionality

---
+++ src/components/DeleteRecipe/DeleteRecipe.tsx
@@ -0,0 +1,31 @@
+
+
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { deleteRecipe } from '../../services/recipeService';
+
+interface RouteParams {
+    id: string;
+}
+
+const DeleteRecipe: React.FC = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        if (id) {
+            await deleteRecipe(parseInt(id, 10));
+            navigate('/');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Are you sure you want to delete this recipe?</h1>
+            <button onClick={handleDelete}>Yes, delete</button>
+            <button onClick={() => navigate(`/edit-recipe/${id}`)}>No, go back</button>
+        </div>
+    );
+};
+
+export default DeleteRecipe;
