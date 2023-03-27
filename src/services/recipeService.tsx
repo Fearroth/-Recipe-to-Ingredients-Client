e6e3870610b0c40304e511ee432c3a0720140a8c
Damian Dusza <damian.dusza@madmountain.io>
2023-03-27 17:30:38 +0200

    Structure

--- src/services/recipeService.tsx
+++ src/services/recipeService.tsx
@@ -1,38 +1,40 @@
 
 import api from './api';
 import { Recipe } from '../types/Recipe';
+import { ApiResources } from '../types/ApiResources';
+import { Endpoints } from '../types/Endpoints';
 
 //get 15 recipes
 export const getRecipes = async (): Promise<Recipe[]> => {
-  const response = await api.get('/recipes/');
-  return response.data.recipes;
+    const response = await api.get(Endpoints.Recpipes);
+    return response.data[ApiResources.Recpipes];
 };
 
 export const getAllRecipes = async (): Promise<Recipe[]> => {
-  const response = await api.get('/recipes/all');
-  return response.data.recipes;
+    const response = await api.get('/recipes/all');
+    return response.data.recipes;
 };
 
 export const getRecipe = async (id: number): Promise<Recipe> => {
-  const response = await api.get(`/recipes/${id}`);
-  return response.data;
+    const response = await api.get(`/recipes/${id}`);
+    return response.data;
 };
 
 export const createRecipe = async (recipe: Recipe): Promise<Recipe> => {
-  const response = await api.post('/recipes/', recipe);
-  return response.data;
+    const response = await api.post('/recipes/', recipe);
+    return response.data;
 };
 
 export const updateRecipe = async (id: number, recipe: Recipe): Promise<Recipe> => {
-  const response = await api.put(`/recipes/${id}`, recipe);
-  return response.data;
+    const response = await api.put(`/recipes/${id}`, recipe);
+    return response.data;
 };
 
 export const deleteRecipe = async (id: number): Promise<void> => {
-  await api.delete(`/recipes/${id}`);
+    await api.delete(`/recipes/${id}`);
 };
 
 export const restoreRecipe = async (id: number): Promise<Recipe> => {
-  const response = await api.put(`/recipes/${id}/restore`);
-  return response.data;
+    const response = await api.put(`/recipes/${id}/restore`);
+    return response.data;
 };
---
+++ src/types/ApiResources.ts
@@ -0,0 +1,3 @@
+export enum ApiResources {
+    Recpipes = 'recipes',
+}
